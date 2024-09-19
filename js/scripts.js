9ccfb879d936af2955250b7e32e4745228b227cd
Josh Butcher <sjwb1999@gmail.com>
2024-09-19 15:36:28 -0700

    add an updated js to js/scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+
 function add(number1, number2) {
   return number1 + number2;
 }
@@ -14,8 +18,8 @@ function subtract(number1, number2) {
   return number1 - number2;
 }
 
-const number1 = prompt("Enter a number: ");
-const number2 = prompt("Enter a number: ");
+const number1 = parseInt(window.prompt("Enter your age: "));
+const number2 = parseInt(window.prompt("Enter your age: "));
+1
 
-
-window.alert(subtract(number1, number2));
+saySomething("The sum is " + add(number1, number2) + ".");
