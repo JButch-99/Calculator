8481cb2577d10bbf307d6053e96a64d26443069f
Josh Butcher <sjwb1999@gmail.com>
2024-09-19 11:49:35 -0700

    add to css/styles.css, bootstrap.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 function add(number1, number2) {
   return number1 + number2;
 }
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
 const number1 = prompt("Enter a number: ");
 const number2 = prompt("Enter a number: ");
 
 
-window.alert(add(number1, number2));
+window.alert(subtract(number1, number2));
